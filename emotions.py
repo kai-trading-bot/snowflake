336fff6b6cf7d3ade1708e2a0ad9aa5634323254
Khizar Qureshi <kqureshi@mit.edu>
2022-02-28 13:19:48 -0800

    update

--- emotions.py
+++ emotions.py
@@ -6,6 +6,10 @@ from typing import *
 __author__ = 'kqureshi'
 
 
+model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+
+
 def get_sentiment(text: str) -> float:
     """ 
     Usage: get_sentiment(text='Today is a great day')
@@ -14,8 +18,6 @@ def get_sentiment(text: str) -> float:
     :param tokenizer:
     :return:
     """
-    model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
-    tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
     inputs = tokenizer.encode_plus(text, return_tensors='pt', add_special_tokens=True)
     token_type_ids = inputs['token_type_ids']
     input_ids = inputs['input_ids']
